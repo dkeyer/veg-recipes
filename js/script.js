76c265f4f59048f1ff922f792faa37d013eece1f
dkeyer <dkeyer@gmail.com>
2021-07-23 12:52:45 -0400

    pushing changes

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-// alert("Yes");
+const form = document.querySelector('form');
+
+const nameInput = document.querySelector(
+    'input[email="email"]'
+);
+
+const validateEmail = () => { 
+    if (!nameInput.value){
+        nameInput.classList.add("invalid");
+        nameInput.nextElementSibling.classlist.remove("hide");
+    }
+};
+
+form.addEventListener('Submit', (e) => {
+    e.preventDefault();
+    validateEmail();
+});
