bbf302941b92e5a654e685089cec03bea9668722
dkeyer <dkeyer@gmail.com>
2021-07-24 19:40:46 -0400

    styling

--- js/script.js
+++ js/script.js
@@ -1,17 +1,18 @@
-const form = document.querySelector('form');
+const form = document.querySelector("form");
 
-const nameInput = document.querySelector(
-    'input[email="email"]'
-);
+console.log(form);
 
-const validateEmail = () => { 
-    if (!nameInput.value){
-        nameInput.classList.add("invalid");
-        nameInput.nextElementSibling.classlist.remove("hide");
-    }
+const validateForm = () => {
+  const emailInput = document.getElementById("emailInput").value;
+  const messageInput = document.getElementById("message").value;
+
+  if (!emailInput.includes("@") || !(emailInput.length > 4)) {
+    alert("Please enter a valid email");
+
+  } else if (!messageInput.length) {
+      alert("Please include a message")
+
+  } else {
+    alert("Thank you for your feedback!");
+  }
 };
-
-form.addEventListener('Submit', (e) => {
-    e.preventDefault();
-    validateEmail();
-});
