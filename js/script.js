a91669d31397a2f8c6095a67f33438313443051a
dkeyer <dkeyer@gmail.com>
2021-07-26 16:34:01 -0400

    new js and styling changes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,126 @@
 const form = document.querySelector("form");
 
-console.log(form);
+const recipes = [
+  {
+    name: "First Recipe",
+    description: "Recipe Description",
+    image: "heron.jpg",
+    ingredients: [
+      "one",
+      "two",
+      "three",
+      "four",
+      "five",
+      "six",
+      "seven",
+      "eight",
+      "nine",
+      "ten",
+    ],
+    steps: ["one", "two", "three", "four", "five", "six", "seven", "eight"],
+  },
+
+  {
+    name: "Second Recipe",
+    description: "Recipe Description",
+    image: "heron.jpg",
+    ingredients: [
+      "one",
+      "two",
+      "three",
+      "four",
+      "five",
+      "six",
+      "seven",
+      "eight",
+      "nine",
+      "ten",
+    ],
+    steps: ["one", "two", "three", "four", "five", "six", "seven", "eight"],
+  },
+  {
+    name: "Third Recipe",
+    description: "Recipe Description",
+    image: "heron.jpg",
+    ingredients: [
+      "one",
+      "two",
+      "three",
+      "four",
+      "five",
+      "six",
+      "seven",
+      "eight",
+      "nine",
+      "ten",
+    ],
+    steps: ["one", "two", "three", "four", "five", "six", "seven", "eight"],
+  },
+  {
+    name: "Fourth Recipe",
+    description: "Recipe Description",
+    image: "heron.jpg",
+    ingredients: [
+      "one",
+      "two",
+      "three",
+      "four",
+      "five",
+      "six",
+      "seven",
+      "eight",
+      "nine",
+      "ten",
+    ],
+    steps: ["one", "two", "three", "four", "five", "six", "seven", "eight"],
+  },
+];
+
+const displayRecipes = () => {
+    let container = document.getElementById("recipeContainer")
+
+    let html = ""
+
+    recipes.map((recipe) => {
+        let ingredientList = ""
+        recipe.ingredients.forEach((ingredient) => {
+            ingredientList += `<li>${ingredient}</li>`
+        })
+
+        let stepList = ""
+        recipe.steps.forEach((step) => {
+            stepList += `<li>${step}</li>`
+        })
+
+
+            html += `<div class="recipe">
+        <div class="recipeHeader">
+            <div class="overlay"></div>
+            <img class="pic" src="${recipe.image}"/>
+            <div class="recipeDescription">
+            <h2>${recipe.name}</h2>
+            <p class="descrip">${recipe.description}</p>
+            </div>
+        </div>
+        <div class="instructions">
+            <div>
+                <h4>Ingredients</h4>
+                <ul class="ingredients">
+                ${ingredientList}
+                </ul>
+            </div>
+            <div>
+                <h4 class="prep">To Prepare the Food...</h4>
+                <ol>
+                ${stepList}
+                </ol>
+            </div>
+        </div>
+    </div>`
+})
+container.innerHTML += html
+}
+displayRecipes()
 
 const validateForm = () => {
   const emailInput = document.getElementById("emailInput").value;
@@ -8,10 +128,8 @@ const validateForm = () => {
 
   if (!emailInput.includes("@") || !(emailInput.length > 4)) {
     alert("Please enter a valid email");
-
   } else if (!messageInput.length) {
-      alert("Please include a message")
-
+    alert("Please include a message");
   } else {
     alert("Thank you for your feedback!");
   }
